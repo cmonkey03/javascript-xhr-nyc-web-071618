f071a7506ea32bd823f21ce1f112ed16bafdb9a8
cmonkey03 <russellpbrown@gmail.com>
2018-09-05 02:45:55 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r =>
+      "<li>" +
+      r.name +
+      ' - <a href="#" data-repo="' +
+      r.name +
+      '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join(" ")}</ul>`;
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitList = `<ul>${commits
+    .map(commit =>
+      "<li><strong>" +
+      commit.author.login +
+      "</strong> - " +
+      commit.commit.message +
+      "</li>"
+    )
+    .join(" ")}</ul>`;
+    document.getElementById('commits').innerHTML = commitList;
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories)
+  req.open("GET", "https://api.github.com/users/cmonkey03/repos");
+  req.send();
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showCommits);
+  req.open("GET", "https://api.github.com/repos/cmonkey03/" + name + "/commits");
+  req.send();
+}
